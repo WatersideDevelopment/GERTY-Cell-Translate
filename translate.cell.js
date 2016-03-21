e9b985fa78ad6335735a1a4e2fd1a411fe17ab3f
D. Rimron-Soutter <darran@xalior.com>
2016-03-21 17:00:40 +0100

    Translations now work! -Dx

--- translate.cell.js
+++ translate.cell.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var gTranslateModule = require('google-translate');
+var q = require('bluebird');
+
 /**
  * Constructor function. It accepts a Bot and settings object which should contain the following keys:
  *
@@ -8,10 +11,12 @@
  *
  * @author D. Rimron-Soutter <darran@xalior.com>
  */
-var TranslateCell = function Constructor(GertyBot, settings) {
-    this.GertyBot = GertyBot
-    this.settings = settings;
+var TranslateCell = function Constructor(GertyBot) {
+    this.GertyBot = GertyBot;
     this.name = 'TranslateCell';
+    this.namespace = 'uk.co.waterside-development.gerty.cells.translate';
+    this.settings = this.GertyBot.settings.cellConfig['uk.co.waterside-development.gerty.cells.translate'];
+    this.translator = gTranslateModule(this.settings.APIKEY);
 };
 
 /**
@@ -22,16 +27,51 @@ var TranslateCell = function Constructor(GertyBot, settings) {
  */
 TranslateCell.prototype.dispatch = function (message) {
     var incoming_command = message.text.match(this.GertyBot.command_detector);
+    var Cell = this;
     switch(incoming_command[0]) {
         case '!translate':
-            this.GertyBot.log.info('Cell.Translate._dispatch: !translate from @'
-                + (this.GertyBot.getUserById(message.user).name));
+            var text = message.text.replace('!translate ','');
+            this.translate(text)
+                .then(function(translation) {
+                    Cell.GertyBot.reply(message, translation);
+                });
+            break;
+        case '!-translate':
+            var text = message.text.replace('!-translate ','');
+
+            this.translate(text, this.settings.default_destination_lang, this.settings.default_source_lang)
+                .then(function(translation) {
+                    Cell.GertyBot.reply(message, translation);
+                });
+            break;
 
     }
 };
 
 TranslateCell.prototype.register = function () {
     this.GertyBot._register('translate', this);
+    this.GertyBot._register('-translate', this);
+};
+
+TranslateCell.prototype.translate = function(text, source_lang, destination_lang) {
+    var deferred = q.defer();
+
+    if(typeof source_lang == 'undefined') {
+        source_lang = this.settings.default_source_lang
+    }
+
+    if(typeof destination_lang == 'undefined') {
+        destination_lang = this.settings.default_destination_lang
+    }
+
+    this.translator.translate(text, source_lang, destination_lang, function(err, translation) {
+        deferred.resolve({
+            text: translation.translatedText
+        });
+    });
+
+    return deferred.promise;
+
 };
 
 module.exports = TranslateCell;
